42bb112f55e69beb6ca8232d8e7a5bf0890def62
Norman <rickey00x@gmail.com>
2023-03-13 11:34:51 +0100

    changed to work with mysql

--- setup.sql
+++ setup.sql
@@ -29,25 +29,25 @@ CREATE TABLE IF NOT EXISTS `ComparisonPrints` (
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE IF NOT EXISTS `Districts` (
-  `disctrict_ID` int NOT NULL AUTO_INCREMENT,
+  `district_ID` int NOT NULL AUTO_INCREMENT,
   `name` varchar(50),
-  PRIMARY KEY (disctrict_ID)
+  PRIMARY KEY (district_ID)
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE IF NOT EXISTS `Groups` (
+CREATE TABLE IF NOT EXISTS `Carbon_Footprint_Groups` (
   `group_ID` int NOT NULL AUTO_INCREMENT,
   `groupname` varchar(50),
   `groupcode` varchar(6) NOT NULL,
   `owner_ID` int,
   PRIMARY KEY (group_ID),
-  FOREIGN KEY (owner_ID) REFERENCES Users(user_ID)
+  FOREIGN KEY (owner_ID) REFERENCES Users(user_ID) ON DELETE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE IF NOT EXISTS `PrintsInDistricts` (
-  `disctrict_ID` int,
+CREATE TABLE IF NOT EXISTS `Prints_In_Districts` (
+  `district_ID` int,
   `print_ID` int,
-  FOREIGN KEY (disctrict_ID) REFERENCES Districts(disctrict_ID),
-  FOREIGN KEY (print_ID) REFERENCES CO2Prints(print_ID)
+  FOREIGN KEY (district_ID) REFERENCES Districts(district_ID) ON DELETE CASCADE,
+  FOREIGN KEY (print_ID) REFERENCES CO2Prints(print_ID) ON DELETE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE IF NOT EXISTS `Groupmemberships` (
@@ -55,12 +55,12 @@ CREATE TABLE IF NOT EXISTS `Groupmemberships` (
   `group_ID` int
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE IF NOT EXISTS `PrintsInGroups` (
+CREATE TABLE IF NOT EXISTS `Prints_In_Groups` (
   `group_ID` int,
   `print_ID` int
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-ALTER TABLE `Groupmemberships` ADD FOREIGN KEY (`user_ID`) REFERENCES `Users`(`user_ID`);
-ALTER TABLE `Groupmemberships` ADD FOREIGN KEY (`group_ID`) REFERENCES `Groups`(`group_ID`);
-ALTER TABLE `PrintsInGroups` ADD FOREIGN KEY (`group_ID`) REFERENCES `Groups`(`group_ID`);
-ALTER TABLE `PrintsInGroups` ADD FOREIGN KEY (`print_ID`) REFERENCES `CO2Prints`(`print_ID`);
+ALTER TABLE `Groupmemberships` ADD FOREIGN KEY (`user_ID`) REFERENCES `Users`(`user_ID`) ON DELETE CASCADE;
+ALTER TABLE `Groupmemberships` ADD FOREIGN KEY (`group_ID`) REFERENCES `Carbon_Footprint_Groups`(`group_ID`) ON DELETE CASCADE;
+ALTER TABLE `Prints_In_Carbon_Footprint_Groups` ADD FOREIGN KEY (`group_ID`) REFERENCES `Carbon_Footprint_Groups`(`group_ID`)ON DELETE CASCADE;
+ALTER TABLE `Prints_In_Carbon_Footprint_Groups` ADD FOREIGN KEY (`print_ID`) REFERENCES `CO2Prints`(`print_ID`)ON DELETE CASCADE;
