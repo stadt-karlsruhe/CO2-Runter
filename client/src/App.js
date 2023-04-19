3616403d10238ceef4e02e619f140eb4e398f70e
theSchmiddi <marco.schmidt03@gmail.com>
2023-04-19 15:22:43 +0200

    Fixing Stepper Added theme.js with example code

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './components/Landingpage/Landingpage';
-import Impressum from './components/Impressum/Impressum';
-import DataPrivacy from './components/DataPrivacy/DataPrivacy';
-import CO2QuestionsDataFetcher from './components/QuestionBlock/CO2QuestionsDataFetcher';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+import LandingPage from "./components/Landingpage/Landingpage";
+import Impressum from "./components/Impressum/Impressum";
+import DataPrivacy from "./components/DataPrivacy/DataPrivacy";
+import CO2QuestionsDataFetcher from "./components/QuestionBlock/CO2QuestionsDataFetcher";
+import theme from "./styles/theme.js";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/CO2Rechner" element={<CO2QuestionsDataFetcher />} />
-        <Route path="/Dashboard" element={<LandingPage />} />
-        <Route path="/datenschutz" element={<DataPrivacy />} />
-        <Route path="/impressum" element={<Impressum />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/CO2Rechner" element={<CO2QuestionsDataFetcher />} />
+          <Route path="/Dashboard" element={<LandingPage />} />
+          <Route path="/datenschutz" element={<DataPrivacy />} />
+          <Route path="/impressum" element={<Impressum />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- client/src/components/QuestionBlock/QuestionCategory.js
+++ client/src/components/QuestionBlock/QuestionCategory.js
@@ -4,29 +4,32 @@ import Stepper from "@mui/material/Stepper";
 import Step from "@mui/material/Step";
 import StepLabel from "@mui/material/StepLabel";
 import QuestionBlock from "./QuestionBlock";
-import { Box, Button, Container } from "@mui/material";
+import { Box, Button, Container, useMediaQuery } from "@mui/material";
 
 const QuestionCategory = (props) => {
   const [activeStep, setActiveStep] = React.useState(0);
   const [isDetailed, setIsDetailed] = React.useState({});
+  const isSmallScreen = useMediaQuery((theme) => theme.breakpoints.down("sm"));
 
   const handleStepChange = (step) => {
     setActiveStep(step);
   };
+
   const handleSwitchChange = (name) => {
     setIsDetailed((prevIsDetailed) => ({
       ...prevIsDetailed,
       [name]: !prevIsDetailed[name],
     }));
   };
-
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 2 }}>
         <Stepper activeStep={activeStep}>
-          {props.category.map((label) => (
+          {props.category.map((label, index) => (
             <Step key={label.name}>
-              <StepLabel>{label.name}</StepLabel>
+              <StepLabel onClick={() => handleStepChange(index)}>
+                {!isSmallScreen && label.name}
+              </StepLabel>
             </Step>
           ))}
         </Stepper>
@@ -42,6 +45,7 @@ const QuestionCategory = (props) => {
           variant="dots"
           steps={props.category.length}
           activeStep={activeStep}
+          position={"static"}
           nextButton={
             <Button
               size="small"
