ae7e22bbd16d1e27d85163c450177c84351daf06
theSchmiddi <marco.schmidt03@gmail.com>
2023-04-19 14:30:13 +0200

    Fixed Detailed Switch

--- client/src/components/QuestionBlock/QuestionBlock.js
+++ client/src/components/QuestionBlock/QuestionBlock.js
@@ -1,37 +1,35 @@
-import React, { useState } from "react";
+import React from "react";
 import Switch from "@mui/material/Switch";
 import Question from "./Question";
-import { Divider, FormGroup, Stack, Typography } from "@mui/material";
+import { Box, Container, Divider, FormGroup, Stack, Typography } from "@mui/material";
 
 const QuestionBlock = (props) => {
-  const [isDetailed, setIsDetailed] = useState(false);
-
-  const handleSwitchChange = () => {
-    setIsDetailed((prevIsDetailed) => !prevIsDetailed);
-  };
-
   return (
-    <div className="question-block-container">
-      <h3>{props.questions.name}</h3>
-      <Divider />
-      {isDetailed ? (
-        props.questions.detailed.questions.map((question) => (
-          <div>
-            <Question question={question} />
-            <Divider sx={{padding:1}} />
-          </div>
-        ))
-      ) : (
-        <Question question={props.questions.quick} />
-      )}
-      <FormGroup>
-        <Stack direction="row" spacing={1} alignItems="center">
-          <Typography>schnell</Typography>
-          <Switch checked={isDetailed} onChange={handleSwitchChange} />
-          <Typography>detailliert</Typography>
-        </Stack>
-      </FormGroup>
-    </div>
+    <Container maxWidth="sm">
+      <Box sx={{ my: 2 }}>
+        <Typography variant="h5" component="div">
+          {props.questions.name}
+        </Typography>
+        <Divider />
+        {props.isDetailed ? (
+          props.questions.detailed.questions.map((question) => (
+            <Box key={question.id}>
+              <Question question={question} />
+              <Divider sx={{ my: 1 }} />
+            </Box>
+          ))
+        ) : (
+          <Question question={props.questions.quick} />
+          )}
+        <FormGroup>
+          <Stack direction="row" spacing={1} alignItems="center">
+            <Typography>schnell</Typography>
+            <Switch checked={props.isDetailed} onChange={props.onSwitchChange} />
+            <Typography>detailliert</Typography>
+          </Stack>
+        </FormGroup>
+      </Box>
+    </Container>
   );
 };
 
--- client/src/components/QuestionBlock/QuestionCategory.js
+++ client/src/components/QuestionBlock/QuestionCategory.js
@@ -4,56 +4,65 @@ import Stepper from "@mui/material/Stepper";
 import Step from "@mui/material/Step";
 import StepLabel from "@mui/material/StepLabel";
 import QuestionBlock from "./QuestionBlock";
+import { Box, Button, Container } from "@mui/material";
 
 const QuestionCategory = (props) => {
   const [activeStep, setActiveStep] = React.useState(0);
+  const [isDetailed, setIsDetailed] = React.useState({});
 
   const handleStepChange = (step) => {
     setActiveStep(step);
   };
 
+  const handleSwitchChange = (name) => {
+    setIsDetailed((prevIsDetailed) => ({
+      ...prevIsDetailed,
+      [name]: !prevIsDetailed[name],
+    }));
+  };
   return (
-    <div>
-      <Stepper activeStep={activeStep}>
-        {props.category.map((label) => (
-          <Step key={label.name}>
-            <StepLabel>{label.name}</StepLabel>
-          </Step>
+    <Container maxWidth="sm">
+      <Box sx={{ my: 2 }}>
+        <Stepper activeStep={activeStep}>
+          {props.category.map((label) => (
+            <Step key={label.name}>
+              <StepLabel>{label.name}</StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+        {props.category[activeStep].questions.map((categoryQuestions) => (
+          <QuestionBlock
+            key={categoryQuestions.name}
+            questions={categoryQuestions}
+            isDetailed={isDetailed[categoryQuestions.name]}
+            onSwitchChange={() => handleSwitchChange(categoryQuestions.name)}
+          />
         ))}
-      </Stepper>
-      {props.category[activeStep].questions.map((categoryQuestions) => (
-        <QuestionBlock questions={categoryQuestions} />
-      ))}
-      <MobileStepper
-        variant="dots"
-        steps={props.category.length}
-        activeStep={activeStep}
-        nextButton={
-          <button
-            onClick={() => handleStepChange(activeStep + 1)}
-            disabled={activeStep === props.category.length - 1}
-          >
-            Next
-          </button>
-        }
-        backButton={
-          <button
-            onClick={() => handleStepChange(activeStep - 1)}
-            disabled={activeStep === 0}
-          >
-            Back
-          </button>
-        }
-      >
-        {props.category.map((label, index) => (
-          <Step key={index}>
-            <StepLabel onClick={() => handleStepChange(index)}>
-              {label.name}
-            </StepLabel>
-          </Step>
-        ))}
-      </MobileStepper>
-    </div>
+        <MobileStepper
+          variant="dots"
+          steps={props.category.length}
+          activeStep={activeStep}
+          nextButton={
+            <Button
+              size="small"
+              onClick={() => handleStepChange(activeStep + 1)}
+              disabled={activeStep === props.category.length - 1}
+            >
+              Next
+            </Button>
+          }
+          backButton={
+            <Button
+              size="small"
+              onClick={() => handleStepChange(activeStep - 1)}
+              disabled={activeStep === 0}
+            >
+              Back
+            </Button>
+          }
+        />
+      </Box>
+    </Container>
   );
 };
 
