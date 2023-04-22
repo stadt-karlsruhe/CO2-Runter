069762d99fc39921aeab332d4d967d41a1f3fb55
theSchmiddi <marco.schmidt03@gmail.com>
2023-04-22 12:40:16 +0200

    Adding defaultValue

--- client/src/components/QuestionBlock/QuestionBlock.js
+++ client/src/components/QuestionBlock/QuestionBlock.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
 import Switch from "@mui/material/Switch";
 import Question from "./Question";
 import {
@@ -12,45 +12,55 @@ import {
 } from "@mui/material";
 
 const QuestionBlock = (props) => {
-  const [co2Values, setCo2Values] = useState([]);
+  const defaultValues = props.questions.detailed.questions.map(
+    (question) => question.defaultValue
+  );
+  const [co2Values, setCo2Values] = useState(defaultValues);
 
-
-  const calculateDetailedCo2 = (values) => {
+  const calculateDetailedCo2 = () => {
     const formulaString = props.questions.detailed.formula;
-    console.log(formulaString)
     const formula = eval(`(${formulaString})`);
-    console.log(values+" : "+formula(values))
-    return formula(values);
-  }
+    console.log(co2Values + " : " + formula(co2Values));
+    return formula(co2Values);
+  };
+
+  useEffect(() => {
+    const co2 = calculateDetailedCo2();
+    props.onCo2ValuesChange(co2);
+  }, [co2Values]);
 
   const handleCo2ValuesChange = (index, value) => {
     const newCo2Values = [...co2Values];
     newCo2Values[index] = value;
     setCo2Values(newCo2Values);
-    const co2 = calculateDetailedCo2(co2Values)
-    props.onCo2ValuesChange(co2)
-  }
-
+  };
 
   return (
     <Container maxWidth="sm">
       <Paper
         sx={{ my: 2, p: 2, backgroundColor: "#f0f0f0", borderRadius: "8px" }}
       >
-        <Typography variant="h5" component="div" sx={{ fontWeight: 'bold' }}>
+        <Typography variant="h5" component="div" sx={{ fontWeight: "bold" }}>
           {props.questions.name}
         </Typography>
         <Divider />
         {props.isDetailed ? (
           props.questions.detailed.questions.map((question, index) => (
             <Box key={index}>
-              <Question question={question} onCo2ValuesChange={(value) => handleCo2ValuesChange(index, value)}/>
+              <Question
+                question={question}
+                onCo2ValuesChange={(value) =>
+                  handleCo2ValuesChange(index, value)
+                }
+              />
               <Divider sx={{ my: 1 }} />
             </Box>
-          )
-          )
+          ))
         ) : (
-          <Question question={props.questions.quick} onCo2ValuesChange={props.onCo2ValuesChange}/>
+          <Question
+            question={props.questions.quick}
+            onCo2ValuesChange={props.onCo2ValuesChange}
+          />
         )}
         <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
           <FormGroup>
