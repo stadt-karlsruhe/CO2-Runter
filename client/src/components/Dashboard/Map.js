f1b81af53ee559c6097165c71ee8976a1fa495cf
Norman <rickey00x@gmail.com>
2023-04-25 18:12:32 +0200

    added map to dashboard

---
+++ client/src/components/Dashboard/Map.js
@@ -0,0 +1,126 @@
+import React, { useState, useEffect } from "react";
+import ReactEcharts from "echarts-for-react";
+import * as echarts from "echarts";
+import geoJson from './districts_geo.json';
+import sampleData from './sampleData.json';
+import axios from 'axios';
+
+echarts.registerMap('Karlsruhe', geoJson);
+
+export default function District_Map() {
+  const [data, setData] = useState(null);
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  useEffect(() => {
+    axios.get('/api/dashboard/footprints')
+      .then((res) => {
+        console.log(res.data);
+        setData({
+          mobility: res.data.mobility,
+          nutrition: res.data.nutrition,
+          consume: res.data.consume,
+          housing: res.data.housing
+        });
+        setDataLoaded(true);
+      })
+      .catch((error) => {
+        console.log(error);
+        setData(sampleData);
+        setDataLoaded(true);
+
+      });
+  }, []);
+
+  const mapOption = {
+    title: {
+      text: 'CO2 Emissionen in Karlsruhe',
+      left: 'center',
+      padding: 10
+    },
+    legend: {
+      top: 35,
+    },
+    tooltip: {
+      trigger: 'item',
+      formatter: '{b}<br/>{c} (kg CO2)'
+    },
+    grid :
+    {
+      left: 'left',
+    },
+    visualMap: {
+      min: 0,
+      max: 20,
+      orient: 'horizontal',
+      left: 'center',
+      bottom: '5%',
+      inRange: {
+        color: ['#313695', '#4575b4', '#74add1', '#abd9e9', '#e0f3f8', '#ffffbf', '#fee090', '#fdae61', '#f46d43', '#d73027', '#a50026']
+      },
+      text: ['High', 'Low'],
+      calculable: true
+    },
+    roam: 'scale',
+    scaleLimit: {
+          min: 1,
+          max: 5
+    },
+    series: dataLoaded ? [
+      {
+        name: 'Mobilität',
+        type: 'map',
+        map: 'Karlsruhe',
+        showLegendSymbol : false,
+        emphasis: {
+          label: {
+            show: true
+          }
+        },
+        data: data.mobility
+
+        }, {
+        name: 'Ernährung',
+        type: 'map',
+        map: 'Karlsruhe',
+        showLegendSymbol : false,
+        emphasis: {
+          label: {
+            show: true
+          }
+        },
+        data: data.nutrition
+      },
+      {
+        name: 'Konsum',
+        type: 'map',
+        map: 'Karlsruhe',
+        showLegendSymbol : false,
+        emphasis: {
+          label: {
+            show: true
+          }
+        },
+        data: data.consume
+      },
+      {
+        name: 'Wohnen',
+        type: 'map',
+        map: 'Karlsruhe',
+        showLegendSymbol : false,
+        data: data.housing
+      }
+    ] : [],
+  
+
+  };
+
+  return (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", width: "80vw", height: "80vh" }}>
+      {dataLoaded ? (
+        <ReactEcharts option={mapOption} style={{ width: "100%", height: "100%" }} />
+      ) : (
+        <div>Loading...</div>
+      )}
+    </div>
+  );
+};
