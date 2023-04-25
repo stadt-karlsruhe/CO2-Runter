3a5cc0640faebdda87aa3d758062b4893af417e6
theSchmiddi <marco.schmidt03@gmail.com>
2023-04-25 21:40:30 +0200

    Added Login for GroupChoice

--- client/src/components/DistrictGroupChoice/FinishScreen.js
+++ client/src/components/DistrictGroupChoice/FinishScreen.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import axios from 'axios';
-import { Tab, Tabs, Typography, Button } from '@mui/material';
-import CityDistrictChoice from './CityDistrictChoice';
-import GroupChoice from './GroupChoice';
-import Login from './Login';
+import React, { useState } from "react";
+import axios from "axios";
+import { Tab, Tabs, Typography, Button } from "@mui/material";
+import CityDistrictChoice from "./CityDistrictChoice";
+import GroupChoice from "./GroupChoice";
+import Login from "./Login";
 
 const FinishScreen = ({ co2ValuesPerCategory }) => {
-  const [tabValue, setTabValue] = React.useState(0);
+  const [tabValue, setTabValue] = useState(0);
+  const [finish, setFinish] = useState(false);
   const isLoggedIn = false; // Hier können Sie den Anmeldestatus des Benutzers überprüfen
 
   const co2Sum = co2ValuesPerCategory.reduce(
@@ -21,36 +22,50 @@ const FinishScreen = ({ co2ValuesPerCategory }) => {
   const handleSubmitData = async () => {
     try {
       // Hier müssen Sie die districts-, groups- und answers-Daten angeben
-      const response = await axios.post('/footprint', {
+      const response = await axios.post("/footprint", {
         districts: {},
         groups: {},
         answers: {},
       });
       if (response.status === 200) {
-        // Erfolgreich gesendet
+        handleContinue();
       }
     } catch (error) {
       console.error(error);
     }
   };
 
+  const handleContinue = () => {
+    setFinish(true);
+  };
+
   return (
     <>
-      <Typography variant="body1">
-        Herzlichen Glückwunsch! Sie haben das Ende erreicht.
-      </Typography>
-      <Typography variant="h4">{co2Sum}</Typography>
-      <Typography variant="body1">
-        Hier ist die Summe Ihrer CO2-Werte pro Kategorie.
-      </Typography>
-      <Tabs value={tabValue} onChange={handleTabChange}>
-        <Tab label="Stadtteile" />
-        <Tab label="Gruppen" />
-      </Tabs>
-      {tabValue === 0 && <CityDistrictChoice />}
-      {tabValue === 1 && (isLoggedIn ? <GroupChoice /> : <Login />)}
-      <Button onClick={handleSubmitData} variant="contained">Daten abschicken</Button>
-      <Button variant="outlined">Weiter ohne Daten zu senden</Button>
+      {!finish ? (
+        <>
+          <Typography variant="body1">
+            Herzlichen Glückwunsch! Sie haben das Ende erreicht.
+          </Typography>
+          <Typography variant="h4">{co2Sum}</Typography>
+          <Typography variant="body1">
+            Hier ist die Summe Ihrer CO2-Werte pro Kategorie.
+          </Typography>
+          <Tabs value={tabValue} onChange={handleTabChange}>
+            <Tab label="Stadtteile" />
+            <Tab label="Gruppen" />
+          </Tabs>
+          {tabValue === 0 && <CityDistrictChoice />}
+          {tabValue === 1 && (isLoggedIn ? <GroupChoice /> : <Login />)}
+          <Button onClick={handleSubmitData} variant="contained">
+            Daten abschicken
+          </Button>
+          <Button onClick={handleContinue} variant="outlined">
+            Weiter ohne Daten zu senden
+          </Button>
+        </>
+      ) : (
+        <></>
+      )}
     </>
   );
 };
--- client/src/components/DistrictGroupChoice/Login.js
+++ client/src/components/DistrictGroupChoice/Login.js
@@ -1,7 +1,35 @@
 import React from "react";
+import LoginForm from "../Login/LoginForm";
+import { Box, Tab, Tabs, Typography } from "@mui/material";
+import { TabContext, TabPanel } from "@mui/lab";
+import GroupChoice from "./GroupChoice";
 
 const Login = () => {
-    return <div>Login folgt</div>
+  const [value, setValue] = React.useState("0");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  return (
+    <Box>
+      <Typography variant="body1">
+        Melden Sie sich an! So können sie alle Gruppen sehen in denen Sie
+        Mitglied sind.
+      </Typography>
+      <TabContext value={value}>
+        <Tabs value={value} onChange={handleChange}>
+          <Tab label="Ohne Login" value="0" />
+          <Tab label="Login" value="1" />
+        </Tabs>
+        <TabPanel value="0">
+          <GroupChoice />
+        </TabPanel>
+        <TabPanel value="1">
+          <LoginForm />
+        </TabPanel>
+      </TabContext>
+    </Box>
+  );
 };
 
 export default Login;
