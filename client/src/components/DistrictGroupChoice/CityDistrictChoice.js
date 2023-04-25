5965108bc56deabd44228843ab92dc32fe1cd541
theSchmiddi <marco.schmidt03@gmail.com>
2023-04-25 20:14:58 +0200

    adding distract choice

--- client/src/components/DistrictGroupChoice/CityDistrictChoice.js
+++ client/src/components/DistrictGroupChoice/CityDistrictChoice.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Autocomplete, TextField } from '@mui/material';
 
 const CityDistrictChoice = () => {
+  const [districts, setDistricts] = useState([]);
 
-    return <></>;
+  useEffect(() => {
+    const fetchDistricts = async () => {
+      try {
+        const response = await axios.get('/api/districts');
+        if (response.status === 200) {
+          setDistricts(response.data);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchDistricts();
+  }, []);
+
+  return (
+    <Autocomplete
+      options={districts}
+      getOptionLabel={(option) => option.name}
+      renderInput={(params) => (
+        <TextField {...params} label="Stadtteil auswählen" />
+      )}
+    />
+  );
 };
 
 export default CityDistrictChoice;
