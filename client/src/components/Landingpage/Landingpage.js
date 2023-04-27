ad187943ed76918b80a481c3a8110769d2c81c57
theSchmiddi <marco.schmidt03@gmail.com>
2023-04-27 09:44:57 +0200

    Update landingpage

--- client/src/components/Landingpage/Landingpage.js
+++ client/src/components/Landingpage/Landingpage.js
@@ -5,6 +5,7 @@ import {
   Accordion,
   AccordionSummary,
   AccordionDetails,
+  Link,
 } from "@mui/material";
 import { ExpandMore } from "@mui/icons-material";
 import Header from "../Header/Header";
@@ -43,8 +44,18 @@ const Landingpage = () => {
             </Typography>
           </AccordionSummary>
           <AccordionDetails>
-            <Typography>Der CO2 Rechner funktioniert wie folgt: ...</Typography>
-          </AccordionDetails>
+            <Typography>
+Laut Energiebilanz Karlsruhe 2019 liegt der CO2 Ausstoß bei 7,8t jährlich pro Kopf. Die Effekte durch privaten Konsum sind dabei nicht enthalten. Addiert man hier den Grundwert (3,5t) gemäß unserer Vereinfachung des UBA-Modells, dann erhält man 11,3t CO2. Dies entspricht ungefähr der Grundwert-Summe der CO2APP (11t CO2).
+
+Diese Grundwert-Summe bildet sich aus 4 "Sektoren", die wir durch unseren Lebensstil beinflussen können plus einer festen Zugabe für öffentliche Infrastruktur.
+
+Wohnen (Heizung und Strom)
+Mobilität
+Ernährung
+Allgemeiner Konsum
+
+Die verwendeten Daten und Berechnungsmodelle sind an den CO2-Rechner des Umweltbundesamts angeleht und wollen die Effekte zeigen, die wir durch sparsamen oder weniger sparsamen Lebensstil erreichen können. Eine exaktere Berechnung ist ohne weitere Informationen über die persönliche Situation im Einzelfall nicht möglich. Weitere Informationen zum UBA-Modell und den Einsparpotentialen finden sich 
+</Typography><Link href="https://www.umweltbundesamt.de/publikationen/klimaneutral-leben"></Link></AccordionDetails>
         </Accordion>
         <Accordion>
           <AccordionSummary
