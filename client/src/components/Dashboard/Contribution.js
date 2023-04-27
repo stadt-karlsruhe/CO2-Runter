62211a3a0aa1f930aa33b7ef67697b4612767006
Norman <rickey00x@gmail.com>
2023-04-27 12:31:35 +0200

    add: diplay of contribution

--- client/src/components/Dashboard/Contribution.js
+++ client/src/components/Dashboard/Contribution.js
@@ -1,27 +1,12 @@
-import { Card, CardContent, Typography } from '@mui/material';
+import Contribution_Total from './Contribution_total';
+import Contribution_per_district from './Contribution_per_district';
 
-export default function Co2Card({ co2Footprint }) {
-    // call a api to get the number of co2Footprint
-    // const [co2Footprint, setCo2Footprint] = useState(0);
-    // useEffect(() => {
-    //     const fetchCo2Footprint = async () => {
-    //         const response = await fetch("/co2Footprint");
-    //         const data = await response.json();
-    //         setCo2Footprint(data);
-    //     };
-    //     fetchCo2Footprint();
-    // }, []);
-    //
+export default function Co2Card() {  
+   
   return (
-    <Card>
-      <CardContent>
-        <Typography variant="h4" align="center">
-          {co2Footprint}
-        </Typography>
-        <Typography variant="subtitle1" align="center">
-          Abgegebene Co2 Fussabdrücke
-        </Typography>
-      </CardContent>
-    </Card>
+    <div>
+        <Contribution_Total />
+        <Contribution_per_district />
+    </div>
   );
 }
