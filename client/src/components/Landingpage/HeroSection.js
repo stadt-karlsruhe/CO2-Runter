e25aed05690f1242bbd3ed01c0429848dccb31e9
theSchmiddi <marco.schmidt03@gmail.com>
2023-04-27 10:40:32 +0200

    Adding demo Data Question

--- client/src/components/Landingpage/HeroSection.js
+++ client/src/components/Landingpage/HeroSection.js
@@ -1,13 +1,14 @@
 import React from "react";
-import {Typography} from "@mui/material";
+import { Typography } from "@mui/material";
 
 const HeroSection = () => {
   return (
     <div className="hero">
       <Typography variant="h1">CO2 Runter</Typography>
       <Typography variant="h5">
-        
-Wie viel CO2 kann ich durch einen veränderten Lebensstil einsparen und wie viel macht es in meinem Stadtteil aus, wenn ich noch andere in meinem Umfeld davon überzeuge?
+        Wie viel CO2 kann ich durch einen veränderten Lebensstil einsparen und
+        wie viel macht es in meinem Stadtteil aus, wenn ich noch andere in
+        meinem Umfeld davon überzeuge?
       </Typography>
     </div>
   );
--- client/src/components/QuestionBlock/CO2QuestionsDataFetcher.js
+++ client/src/components/QuestionBlock/CO2QuestionsDataFetcher.js
@@ -3,18 +3,24 @@ import { Box, Container, Typography } from "@mui/material";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import QuestionCategory from "../QuestionBlock/QuestionCategory";
+import DemoQuestion from "../../DemoQuestion.json"
 import axios from 'axios';
 
 const CO2QuestionsDataFetcher = () => {
   const [questions, setQuestions] = useState("");
 
   useEffect(() => {
-      const fetchQuestions = async () => {
-      const response = await axios.get('/api/questions');
-      const data = response.data;
-      setQuestions(data);
+    const fetchQuestions = async () => {
+      try {
+        const response = await axios.get('/api/questions');
+        const data = response.data;
+        setQuestions(data);
+      } catch (error) {
+        //Nur für übungszwecke
+        setQuestions(DemoQuestion);
+      }
     };
-
+  
     fetchQuestions();
   }, []);
 
