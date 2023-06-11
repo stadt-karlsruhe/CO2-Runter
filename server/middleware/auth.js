c9023d54a36678a2d39aea3d1f00ecfb2436d091
Norman <rickey00x@gmail.com>
2023-06-11 17:31:52 +0200

    Revert "small fixes"
    This reverts commit 19f64c50922f2c910a1f6dceb98b2eda88a52988.

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,25 +3,18 @@ const jwt = require('jsonwebtoken');
 const config = process.env;
 
 const auth = (req, res, next) => {
-  const token = req.body.token || req.query.token || req.headers['x-access-token'];
-  if (!token) {
-    return res.status(401).json({ error: 'Access denied. No token provided.' });
-  }
-
-  try {
-    const decoded = jwt.verify(token, config.TOKEN_KEY);
-    req.user = decoded;
-    next();
-  } catch (err) {
-    if (err.name === 'TokenExpiredError') {
-      return res.status(401).json({ error: 'Token has expired.' });
+    const token =  req.body.token || req.query.token || req.headers["x-access-token"];
+    if (!token) 
+        return res.status(401).send('Access denied. No token provided.');
+    
+    try {
+        const decoded = jwt.verify(token, config.TOKEN_KEY);
+        req.user = decoded;
+        next();
     }
-    if (err.name === 'JsonWebTokenError') {
-      return res.status(401).json({ error: 'Invalid token.' });
+    catch (ex) {
+        res.status(400).send('Invalid token.');
     }
-    // Handle other JWT verification errors if necessary
-    return res.status(401).json({ error: 'Failed to authenticate token.' });
-  }
-};
+    }
 
 module.exports = auth;
