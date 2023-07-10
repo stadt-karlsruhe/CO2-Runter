a253cfca53ad346426ca377ab7bce3c2df87ea0b
Andreas Kugel <devel@akugel.de>
2023-07-10 16:40:13 +0200

    default values for comparison prints. Load after setup.sql

---
+++ defaults.sql
@@ -0,0 +1,4 @@
+-- Test data for ComparisonPrints table
+Insert into ComparisonPrints (name, mobility, housing, consume, nutrition, totalPrint, date) Values ("Deutschland", 2.2, 2.7, 3.4, 1.7, 10, "2020-01-01");
+Insert into ComparisonPrints (name, mobility, housing, consume, nutrition, totalPrint, date) Values ("Karlsruhe", 2.32, 3.36, 3.45, 1.89, 11, "2020-01-02");
+
