43fb433481c6a496f5aab51127c014b8a8ef6e14
Kevin Beier <beierkevin2002@outlook.com>
2024-03-07 23:07:44 +0100

    updated navigation bar

--- client/src/components/TheNavigationHeader.vue
+++ client/src/components/TheNavigationHeader.vue
@@ -1,22 +1,73 @@
-<script setup lang="ts"></script>
-
 <template>
     <v-app-bar rounded>
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-app-bar-nav-icon
+            class="hidden-md-and-up ms-md-3 ms-sm-5 ms-3 text-muted"
+            @click.capture="isDrawerOpen = !isDrawerOpen"
+        ></v-app-bar-nav-icon>
 
-        <v-toolbar-title>CO2Runter </v-toolbar-title>
+        <img
+            src="../../public/CO2RunterLogo.ico"
+            class="hidden-md-and-down ms-md-3 ms-sm-5 ms-3 text-muted"
+        />
+        <v-toolbar-title> CO2Runter </v-toolbar-title>
 
-        <v-btn href="/">Home</v-btn>
-        <v-btn href="/dashboard">Dashboard</v-btn>
-        <v-btn href="/calculator">Calculator</v-btn>
+        <div class="hidden-sm-and-down">
+            <v-btn rounded href="/">Home</v-btn>
+            <v-btn rounded href="/calculator">CO2 Rechner</v-btn>
+            <v-btn rounded href="/dashboard">Dashboard</v-btn>
+            <v-btn rounded href="/info">Informationen</v-btn>
+            <v-btn rounded href="/faq">FAQ</v-btn>
 
-        <v-spacer></v-spacer>
+            <v-btn
+                rounded
+                color="primary-darken-1"
+                variant="tonal"
+                href="/gruppensystem"
+                >Gruppensystem
+            </v-btn>
 
-        <template v-slot:append>
-            <v-btn icon="mdi-magnify"></v-btn>
-            <v-btn icon="mdi-cog"></v-btn>
-        </template>
+            <v-btn
+                rounded
+                color="primary-darken-1"
+                variant="tonal"
+                href="/login"
+                >Login
+            </v-btn>
+        </div>
     </v-app-bar>
+
+    <v-navigation-drawer app v-model="isDrawerOpen" class="hidden-md-and-up">
+        <!-- Drawer content -->
+        <v-list>
+            <v-list-item link href="/">
+                <v-list-item-title>Home</v-list-item-title>
+            </v-list-item>
+            <v-list-item link href="/calculator">
+                <v-list-item-title>CO2 Rechner</v-list-item-title>
+            </v-list-item>
+            <v-list-item link href="/dashboard">
+                <v-list-item-title>Dashboard</v-list-item-title>
+            </v-list-item>
+            <v-list-item link href="/info">
+                <v-list-item-title>Informationen</v-list-item-title>
+            </v-list-item>
+            <v-list-item link href="/faq">
+                <v-list-item-title>FAQ</v-list-item-title>
+            </v-list-item>
+            <v-list-item link href="/gruppensystem">
+                <v-list-item-title>Gruppensystem</v-list-item-title>
+            </v-list-item>
+            <v-list-item link href="/login">
+                <v-list-item-title>Login</v-list-item-title>
+            </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const isDrawerOpen = ref(false);
+</script>
 
 <style scoped></style>
