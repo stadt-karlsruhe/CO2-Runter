2add43fd05e0849332391bd156a98899d8e73f2b
Kevin Beier <beierkevin2002@outlook.com>
2024-03-07 23:07:29 +0100

    created custom vuetify color theme

--- client/src/plugins/vuetify.ts
+++ client/src/plugins/vuetify.ts
@@ -12,15 +12,49 @@ import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+
+const myCustomLightTheme = {
+    dark: false,
+    colors: {
+        background: '#FFFFFF',
+        surface: '#FFFFFF',
+        'surface-bright': '#FFFFFF',
+        'surface-light': '#EEEEEE',
+        'surface-variant': '#424242',
+        'on-surface-variant': '#EEEEEE',
+        primary: '#D0ECDE',
+        'primary-darken-1': '#01502D',
+        secondary: '#48A9A6',
+        'secondary-darken-1': '#018786',
+        error: '#B00020',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FB8C00',
+    },
+    variables: {
+        'border-color': '#000000',
+        'border-opacity': 0.12,
+        'high-emphasis-opacity': 0.87,
+        'medium-emphasis-opacity': 0.6,
+        'disabled-opacity': 0.38,
+        'idle-opacity': 0.04,
+        'hover-opacity': 0.04,
+        'focus-opacity': 0.12,
+        'selected-opacity': 0.08,
+        'activated-opacity': 0.12,
+        'pressed-opacity': 0.12,
+        'dragged-opacity': 0.08,
+        'theme-kbd': '#212529',
+        'theme-on-kbd': '#FFFFFF',
+        'theme-code': '#F5F5F5',
+        'theme-on-code': '#000000',
+    },
+};
 export default createVuetify({
     theme: {
+        defaultTheme: 'myCustomLightTheme',
         themes: {
-            light: {
-                colors: {
-                    primary: '#1867C0',
-                    secondary: '#5CBBF6',
-                },
-            },
+            myCustomLightTheme,
         },
     },
 });
