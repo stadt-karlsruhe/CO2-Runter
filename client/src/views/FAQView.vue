1ec91243d2269a7ee269ee0337f8feabad9aca68
Julian Stadler <jstadler@testo.de>
2024-03-14 16:48:43 +0100

    implement faq view with sample data

--- client/src/views/FAQView.vue
+++ client/src/views/FAQView.vue
@@ -21,22 +21,99 @@
                 />
             </v-col>
         </v-row>
-
-        <h1 class="text-primary-darken-1 text-center">Literaturliste</h1>
-
-        <v-card>
-            <v-data-table :items="literatureSources"></v-data-table>
-        </v-card>
+        <v-row justify="center">
+            <h1 class="text-primary-darken-1 text-center">FAQ</h1>
+            <v-expansion-panels variant="popout">
+                <v-expansion-panel
+                    title="Warum sollte ich das Klima schützen?"
+                    text="Lorem ipsum..."
+                    class="text-secondary"
+                ></v-expansion-panel>
+                <v-expansion-panel
+                    title="Was ist der CO2 Fußabdruck und wieso ist er so wichtig?"
+                    text="Lorem ipsum..."
+                    class="text-secondary"
+                ></v-expansion-panel>
+                <v-expansion-panel
+                    title="Wie kann ich den Klimaschutz in meinen Alltag integrieren?"
+                    text="Lorem ipsum..."
+                    class="text-secondary"
+                ></v-expansion-panel>
+                <v-expansion-panel
+                    title="Wo kann ich mich weiter informieren?"
+                    text="Lorem ipsum..."
+                    class="text-secondary"
+                ></v-expansion-panel>
+            </v-expansion-panels>
+        </v-row>
+        <v-row>
+            <v-container style="padding-top: 7em">
+                <h1 class="text-primary-darken-1 text-center">
+                    Literaturliste
+                </h1>
+                <v-card>
+                    <v-data-table :items="literatureSources"></v-data-table>
+                </v-card>
+            </v-container>
+        </v-row>
     </v-container>
 </template>
 
 <script setup lang="ts">
 const literatureSources = [
     {
-        title: 'Test Titel eines Buches',
-        author: 'Test Name',
-        source: 'Quelle des Textes/Internetseite',
-        year: 2019,
+        titel: 'Test Titel eines Buches',
+        autor: 'Test Name',
+        jahr: 'Quelle des Textes/Internetseite',
+        quelle: 2019,
+    },
+    {
+        titel: 'Test Titel eines Buches',
+        autor: 'Test Name',
+        jahr: 'Quelle des Textes/Internetseite',
+        quelle: 2019,
+    },
+    {
+        titel: 'Test Titel eines Buches',
+        autor: 'Test Name',
+        jahr: 'Quelle des Textes/Internetseite',
+        quelle: 2019,
+    },
+    {
+        titel: 'Test Titel eines Buches',
+        autor: 'Test Name',
+        jahr: 'Quelle des Textes/Internetseite',
+        quelle: 2019,
+    },
+    {
+        titel: 'Test Titel eines Buches',
+        autor: 'Test Name',
+        jahr: 'Quelle des Textes/Internetseite',
+        quelle: 2019,
+    },
+    {
+        titel: 'Test Titel eines Buches',
+        autor: 'Test Name',
+        jahr: 'Quelle des Textes/Internetseite',
+        quelle: 2019,
+    },
+    {
+        titel: 'Test Titel eines Buches',
+        autor: 'Test Name',
+        jahr: 'Quelle des Textes/Internetseite',
+        quelle: 2019,
+    },
+    {
+        titel: 'Test Titel eines Buches',
+        autor: 'Test Name',
+        jahr: 'Quelle des Textes/Internetseite',
+        quelle: 2019,
+    },
+    {
+        titel: 'Test Titel eines Buches',
+        autor: 'Test Name',
+        jahr: 'Quelle des Textes/Internetseite',
+        quelle: 2019,
     },
 ];
 </script>
