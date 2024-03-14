2118cfeda9df8816a66d36b113947e78a6fa35d0
Julian Stadler <jstadler@testo.de>
2024-03-14 10:24:49 +0100

    implementet first section of faq-view

--- client/src/views/FAQView.vue
+++ client/src/views/FAQView.vue
@@ -1,5 +1,27 @@
+<template>
+    <v-container justify="center">
+        <v-row class="d-flex flex-column-reverse flex-md-row my-16">
+            <v-col cols="12" md="7" class="text-center text-md-start">
+                <h1 class="text-primary-darken-1">
+                    Sie haben Fragen oder wollen sich weiter informieren?
+                </h1>
+                <p class="text-secondary my-8">
+                    Hier finden Sie weitere Infos rund um die CO2Runter
+                    Webseite. Zusätzlich finden Sie hier unsere Quellen und
+                    weitere Artikel. Stöbern Sie gerne auf den verlinkten
+                    Seiten, um weitere Informationen rund um den Klimaschutz zu
+                    erhalten.
+                </p>
+            </v-col>
+            <v-col class="d-flex justify-center" cols="12" md="5">
+                <v-img
+                    width="360px"
+                    height="200px"
+                    src="../assets/undraw_question_for_faq.svg"
+                />
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
 <script setup lang="ts"></script>
-
-<template>FAQ</template>
-
-<style scoped></style>
