6fbba5a8eed4cff4603b7d8183b49f144d08eac6
Julian Stadler <jstadler@testo.de>
2024-03-14 18:04:09 +0100

    implement another section in faq page

--- client/src/views/FAQView.vue
+++ client/src/views/FAQView.vue
@@ -1,16 +1,15 @@
 <template>
     <v-container justify="center">
         <v-row class="d-flex flex-column-reverse flex-md-row my-16">
+            <!-- First Row -->
             <v-col cols="12" md="7" class="text-center text-md-start">
                 <h1 class="text-primary-darken-1">
-                    Sie haben Fragen oder wollen sich weiter informieren?
+                    Sie haben Fragen zum Klimaschutz oder zur Webseite??
                 </h1>
                 <p class="text-secondary my-8">
                     Hier finden Sie weitere Infos rund um die CO2Runter
-                    Webseite. Zusätzlich finden Sie hier unsere Quellen und
-                    weitere Artikel. Stöbern Sie gerne auf den verlinkten
-                    Seiten, um weitere Informationen rund um den Klimaschutz zu
-                    erhalten.
+                    Webseite. Zusätzlich finden Sie hier Antworten und
+                    Erklärungen rund um das Thema Klimaschutz.
                 </p>
             </v-col>
             <v-col class="d-flex justify-center" cols="12" md="5">
@@ -22,6 +21,7 @@
             </v-col>
         </v-row>
         <v-row justify="center">
+            <!-- Second Row -->
             <h1 class="text-primary-darken-1 text-center">FAQ</h1>
             <v-expansion-panels variant="popout">
                 <v-expansion-panel
@@ -46,13 +46,37 @@
                 ></v-expansion-panel>
             </v-expansion-panels>
         </v-row>
+        <v-row style="padding-top: 7em">
+            <!-- Third Row -->
+            <v-col class="d-flex justify-center" cols="12" md="5">
+                <v-img
+                    width="360px"
+                    height="200px"
+                    src="../assets/undraw_question_for_faq.svg"
+                />
+            </v-col>
+            <v-col cols="12" md="7" class="text-center text-md-start">
+                <h1 class="text-primary-darken-1">
+                    Sie möchten sich selbst ein wenig tiefer in den Klimaschutz
+                    einlesen oder unsere Quellen sehen?
+                </h1>
+                <p class="text-secondary my-8">
+                    Dann finden Sie hier unsere Literaturliste. Anbei sind alle
+                    Informationen aufgelistet, die auf der CO2Runter Webseite
+                    präsentiert werden. Zusätzlich sind weitere interessante
+                    Artikel zum Thema Klimaschutz enthalten. Schauen Sie
+                    unbedingt rein!
+                </p>
+            </v-col>
+        </v-row>
         <v-row>
-            <v-container style="padding-top: 7em">
+            <!-- Fourth Row -->
+            <v-container>
                 <h1 class="text-primary-darken-1 text-center">
                     Literaturliste
                 </h1>
                 <v-card>
-                    <v-data-table :items="literatureSources"></v-data-table>
+                    <v-data-table :items="literatureSources"> </v-data-table>
                 </v-card>
             </v-container>
         </v-row>
