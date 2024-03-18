0d3f9aa4e9cd31deee940c6a27799aa3d8442cf9
Kevin Beier <beierkevin2002@gmail.com>
2024-03-18 22:35:46 +0100

    created a little bit from the co2 contribution page

---
+++ client/src/components/Dashboard/Co2PerDistrictContribution.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>Per District Contribution</template>
+
+<style scoped></style>
---
+++ client/src/components/Dashboard/Co2TotalContribution.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-card>
+        <v-card-text v-if="isLoading" class="text-center">
+            <h1 class="text-primary-darken-1">{{ totalCo2Footprint }}</h1>
+            <p>Abgegebene CO2-Fussabdrücke</p>
+        </v-card-text>
+        <v-card-text v-else class="text-center">
+            <p>Daten werden geladen</p>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import { useFetch } from '@vueuse/core';
+
+const totalCo2Footprint = ref(0);
+const fetchError = ref(null);
+const isLoading = ref(true);
+
+onMounted(async () => {
+    const { isFetching, error, data } = await useFetch(
+        '/api/foodprint/total'
+    ).json();
+    isLoading.value = isFetching;
+
+    // Check if there was an error with the fetch request
+    if (error.value) {
+        fetchError.value = 'An error occurred while fetching data.';
+    } else {
+        totalCo2Footprint.value = data.value[0].total;
+    }
+});
+</script>
