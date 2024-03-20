d4df20e46c8b3f07fdf148efb372dacbfc038bc4
Kevin Beier <beierkevin2002@gmail.com>
2024-03-20 12:54:48 +0100

    improved creating groups page design

--- client/src/views/GroupSystemNewGroupView.vue
+++ client/src/views/GroupSystemNewGroupView.vue
@@ -7,34 +7,55 @@
             />
         </v-container>
 
-        <v-container v-else class="mt-5">
-            <v-row>
-                <v-col cols="12" sm="6" md="4">
+        <v-container v-else justify="center">
+            <v-row class="d-flex flex-column-reverse flex-md-row my-16">
+                <v-col cols="12" md="7" class="text-center text-md-start">
+                    <h1 class="text-primary-darken-1">
+                        Eine neue Gruppe erstellen
+                    </h1>
+
+                    <div class="my-8">
+                        <p>
+                            Um eine neue Gruppe zu erstellen, müssen Sie
+                            lediglich eine Gruppenname eingeben und auf "Gruppe
+                            erstellen" klicken.
+                        </p>
+                    </div>
+
                     <v-text-field
+                        prepend-inner-icon="mdi-account-group-outline"
                         v-model="groupName"
                         :clearable="true"
-                        outlined
-                        label="Gruppenname"
+                        variant="outlined"
+                        label="Gruppen Name"
+                        :error-messages="error"
                         :disabled="isFetching"
                     ></v-text-field>
-                </v-col>
-            </v-row>
 
-            <v-row>
-                <v-col cols="12" sm="6" md="4">
                     <v-btn
+                        variant="tonal"
+                        :rounded="true"
+                        color="primary-darken-1"
+                        append-icon="mdi-chevron-right"
+                        size="large"
                         :disabled="isFetching || !groupName"
                         @click="CreateGroup()"
                     >
                         Gruppe erstellen
                     </v-btn>
                 </v-col>
+                <v-col class="d-flex align-center justify-center" cols="12" md="5">
+                    <v-img
+                        width="360px"
+                        height="200px"
+                        src="../assets/undraw_selecting_team_re_ndkb.svg"
+                    />
+                </v-col>
             </v-row>
-
             <v-row v-if="error">
-                <v-col cols="12" sm="6" md="4">
-                    <v-alert type="error">
-                        {{ error }}
+                <v-col>
+                    <v-alert icon="mdi-alert" type="error">
+                        {{ error }}Alert
                     </v-alert>
                 </v-col>
             </v-row>
@@ -43,8 +64,6 @@
 </template>
 
 <script setup lang="ts">
-//TODO: IMPROVE DESIGN
-
 import { ref } from 'vue';
 import { useFetch } from '@vueuse/core';
 import GroupSuccesfull from '@/components/GroupSystem/GroupSuccesfull.vue';
