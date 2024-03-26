4be3ced01a67c9a74bf2d1a535fdf2d904b81ec5
Julian Stadler <jstadler@testo.de>
2024-03-26 16:14:15 +0100

    grundgerüst für co2rechner implementiert

---
+++ client/src/components/Calculator/QuestionsBlock.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-sheet border>
+        <v-card-title> Wie häufig fahren Sie mit dem Auto? </v-card-title>
+        <v-select
+            :items="['Nie', 'Selten', 'Normal', 'Häufig', 'Sehr häufig']"
+            variant="outlined"
+            rounded
+            placeholder="Wählen Sie eine Option"
+            class="ma-8"
+        >
+        </v-select>
+    </v-sheet>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        items: ['Nie', 'Selten', 'Normal', 'Häufig', 'Sehr Häufig'],
+    }),
+};
+</script>
+
+<style scoped>
+*v-select {
+    width: 70%;
+}
+</style>
