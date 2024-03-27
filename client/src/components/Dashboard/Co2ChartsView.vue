b41b9ef26f188591299c36c1312f2bb7c2b3b821
Kevin Beier <beierkevin2002@gmail.com>
2024-03-27 16:54:37 +0100

    added sample graph and link to the page which has to be done still

--- client/src/components/Dashboard/Co2ChartsView.vue
+++ client/src/components/Dashboard/Co2ChartsView.vue
@@ -1,4 +1,77 @@
-<template>Chart</template>
+<template>
+    <v-card>
+        <v-card-title>Chart</v-card-title>
+        <v-card-text>
+            <v-chart
+                :option="chartOptions"
+                autoresize
+                :loading="loading"
+                :loadingOptions="loadingOptions"
+                style="height: 600px; width: 100%"
+            />
+        </v-card-text>
+    </v-card>
+</template>
+
+<script setup lang="ts">
+// TODO: https://github.com/stadt-karlsruhe/CO2-Runter/blob/main/client/src/components/Dashboard/ChartAvg.js
+import { shallowRef, onMounted, ref } from 'vue';
+import { use } from 'echarts/core';
+import { BarChart } from 'echarts/charts';
+import { DatasetComponent, GridComponent } from 'echarts/components';
+import { CanvasRenderer } from 'echarts/renderers';
+import VChart from 'vue-echarts';
+
+use([BarChart, DatasetComponent, GridComponent, CanvasRenderer]);
+
+interface DistrictFootprint {
+    name: string;
+    total: number;
+}
+
+const footprints_in_districts = ref<DistrictFootprint[]>([]);
+const chartOptions = shallowRef(getData());
+const loading = shallowRef(false);
+const loadingOptions = {
+    text: 'Loading…',
+    color: '#4ea397',
+    maskColor: 'rgba(255, 255, 255, 0.4)',
+};
+
+const fetchFootprintsInDistricts = async () => {
+    const response = await fetch('/api/foodprint/districts');
+    const data: DistrictFootprint[] = await response.json();
+    return data;
+};
+
+onMounted(async () => {
+    footprints_in_districts.value = await fetchFootprintsInDistricts();
+    chartOptions.value = getData();
+});
+
+function getData() {
+    return {
+        textStyle: {
+            fontFamily: 'Inter, "Helvetica Neue", Arial, sans-serif',
+            fontWeight: 300,
+        },
+        dataset: {
+            dimensions: ['name', 'total'],
+            source: footprints_in_districts.value.map(
+                (districtFootprint: DistrictFootprint) => ({
+                    name: districtFootprint.name,
+                    total: districtFootprint.total,
+                })
+            ),
+        },
+        xAxis: {},
+        yAxis: { type: 'category' },
+        series: [{ type: 'bar' }],
+    };
+}
+</script>
+
+<style scoped></style>
 
 <!--<template>-->
 <!--    <div v-if="isLoading" style="text-align: center">-->
@@ -91,4 +164,3 @@
 <!--    height: 100vh;-->
 <!--}-->
 <!--</style>-->
-<script setup lang="ts"></script>
