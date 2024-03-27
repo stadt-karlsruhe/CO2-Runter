8b8af10af7153a215fb758f7fe97f264bf8b9122
Kevin Beier <beierkevin2002@outlook.com>
2024-03-27 11:29:59 +0100

    added simple ui to join a group (currently not working)

--- client/src/views/GroupSystemView.vue
+++ client/src/views/GroupSystemView.vue
@@ -1,5 +1,12 @@
 <template>
     <v-container justify="center">
+        <v-row v-if="error">
+            <v-col>
+                <v-alert v-if="error" icon="mdi-alert" type="error">
+                    {{ error }}</v-alert
+                >
+            </v-col>
+        </v-row>
         <v-row class="d-flex flex-column-reverse flex-md-row my-16">
             <v-col cols="12" md="7" class="text-center text-md-start">
                 <h1 class="text-primary-darken-1">Das Gruppensystem</h1>
@@ -14,6 +21,26 @@
                         So können Sie sich gegenseitig zu weiteren CO2
                         Einsparungen anregen.
                     </p>
+
+                    <div class="d-flex justify-center align-center my-4">
+                        <v-text-field
+                            v-model="groupCode"
+                            label="Gruppen-Code"
+                            variant="outlined"
+                        />
+
+                        <v-btn
+                            variant="tonal"
+                            :rounded="true"
+                            color="primary-darken-1"
+                            append-icon="mdi-chevron-right"
+                            size="large"
+                            @click="joinGroup()"
+                        >
+                            Gruppe Hinzufügen
+                        </v-btn>
+                    </div>
+
                 </div>
 
                 <div class="my-8">
@@ -60,6 +87,36 @@
     </v-container>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const groupCode = ref('');
+const error = ref('');
+
+// TODO: this is not working currently
+const joinGroup = async() => {
+    try {
+        const response = await fetch('/add_user', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                groupcode: groupCode.value,
+            }),
+        });
+        console.log(response);
+        if (!response.ok) throw new Error(await response.text());
+        const message = await response.text();
+        error.value = '';
+
+        // Reset groupCode
+        groupCode.value = '';
+    } catch (err) {
+        console.error(err);
+        error.value = 'Fehler beim Hinzufügen zur Gruppe';;
+    }
+};
+</script>
 
 <style scoped></style>
