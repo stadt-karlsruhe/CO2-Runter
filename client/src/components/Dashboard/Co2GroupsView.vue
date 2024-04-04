51042a5be73ce84a8a858fbe0d6ad5239ca025e5
Kevin Beier <beierkevin2002@gmail.com>
2024-04-04 18:35:47 +0200

    first try to recreate the group dashboard page

--- client/src/components/Dashboard/Co2GroupsView.vue
+++ client/src/components/Dashboard/Co2GroupsView.vue
@@ -1,76 +1,242 @@
 <template>
     <v-card>
-        <v-card-title>Gruppen</v-card-title>
+        <v-card-title>Chart</v-card-title>
         <v-card-text>
-            <v-progress-circular class="mx-auto"></v-progress-circular>
+            <div v-if="isLoading">Daten werden geladen...</div>
+
+            <div v-if="error">Es ist ein fehler aufgetreten: error</div>
+
             <v-chart
-                :option="chartOptions"
+                v-if="chartOptions"
+                :option="chartOptions!"
                 autoresize
-                :loading="loading"
+                :loading="isLoading"
                 :loadingOptions="loadingOptions"
                 style="height: 600px; width: 100%"
             />
 
-            <div style="text-align: center">
-                Keine Daten verfügbar: Wähle eine Gruppe aus oder füge eine Neue
-                hinzu
-            </div>
+            <v-select
+                v-model="selectedGroups"
+                :items="groups.map((group) => group.groupname)"
+                :multiple="true"
+                label="Angezeigte Gruppen wählen"
+                @update:modelValue="updateChartOptions()"
+            >
+            </v-select>
 
-            <v-select label="Angezeigte Gruppen wählen"></v-select>
+            <p>Du willst einer Gruppe beitreten, dann geh zum Gruppensysten und tritt mit dem Code bei</p>
         </v-card-text>
     </v-card>
 </template>
 
 <script setup lang="ts">
-// TODO: https://github.com/stadt-karlsruhe/CO2-Runter/blob/main/client/src/components/Dashboard/ChartGroups.js
-import { shallowRef, onMounted, ref } from 'vue';
+import { ComparisonPrints } from '@/types/ComparisonPrints';
+import { AverageCo2Emissions } from '@/types/AverageCo2Emissions';
+import { onMounted, ref } from 'vue';
 import { use } from 'echarts/core';
 import { BarChart } from 'echarts/charts';
 import { DatasetComponent, GridComponent } from 'echarts/components';
 import { CanvasRenderer } from 'echarts/renderers';
 import VChart from 'vue-echarts';
-import { DistrictFootprint } from '@/components/Dashboard/DistrictFootprint';
+import {Group} from "@/types/Group";
 
 use([BarChart, DatasetComponent, GridComponent, CanvasRenderer]);
 
-const footprints_in_districts = ref<DistrictFootprint[]>([]);
-const chartOptions = shallowRef(getData());
-const loading = shallowRef(false);
+const selectedGroups = ref([]);
+const isLoading = ref(true);
+const averageData = ref<AverageCo2Emissions>([]);
+const footprintsData = ref<ComparisonPrints>([]);
+const error = ref('');
+
+
+const groups = ref<Array<Group>>([]);
+const fetchGroups = async () => {
+    isLoading.value = true;
+    try {
+        const response = await fetch('/api/groups/member', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                co2token: `${localStorage.getItem('CO2Token')}`,
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        groups.value = data as Array<Group>;
+        console.log(groups.value);
+    } catch (fetchError) {
+        console.error('Request failed: ', fetchError);
+    }
+    isLoading.value = false;
+};
+
+const fetchData = async () => {
+    isLoading.value = true;
+    const average = await fetch('/api/dashboard/footprints/average');
+    averageData.value = await average.json();
+    const footprints = await fetch('/api/dashboard/comparisonprints');
+    footprintsData.value = await footprints.json();
+    isLoading.value = false;
+};
+
+const fetchFootprintsForAllAvailableGroups = async () => {
+    isLoading.value = true;
+    try {
+        for (let i = 0; i < groups.value.length; i++) {
+            const response = await fetch(`/api/dashboard/groupfootprint?groupcode=${groups.value[i].groupcode}`, {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                }
+            });
+            if (response.status === 200) {
+                const data = await response.json();
+                console.log(data);
+                if (data.values && data.values.length > 0) {
+                    const groupExists = footprintsData.value.some((groupFootprint: any) => groupFootprint.name ===
+                        data.name);
+                    if (!groupExists) {
+                        footprintsData.value = [...footprintsData.value, data];
+                    }
+                }
+            }
+        }
+    } catch (error) {
+        console.error(error);
+    } finally {
+        isLoading.value = false;
+    }
+};
+
+onMounted(async () => {
+    await fetchData();
+    await fetchGroups();
+    await fetchFootprintsForAllAvailableGroups();
+
+    if (footprintsData.value != null && footprintsData.value.length > 0) {
+        chartOptions.value = getData();
+    }
+});
+
+const updateChartOptions = () => {
+    const data = getData();
+    if (data) {
+        chartOptions.value = data;
+    }
+};
+
 const loadingOptions = {
     text: 'Loading…',
     color: '#4ea397',
     maskColor: 'rgba(255, 255, 255, 0.4)',
 };
 
-const fetchFootprintsInDistricts = async () => {
-    const response = await fetch('/api/foodprint/districts');
-    const data: DistrictFootprint[] = await response.json();
-    return data;
-};
-
-onMounted(async () => {
-    footprints_in_districts.value = await fetchFootprintsInDistricts();
-    chartOptions.value = getData();
-});
+const chartOptions = ref<any>(null);
 
 function getData() {
+    console.log(footprintsData.value);
+    if (footprintsData.value.length === 0 || averageData.value.length === 0) {
+        return null;
+    }
+
+    const selectedData = selectedGroups.value.map((selectedGroup) => {
+        const footprint = footprintsData.value.find(
+            (f) => f.name === selectedGroup
+        );
+        return footprint!.values.map((valueObj) => ({
+            category: valueObj.category,
+            value: valueObj.value,
+        }));
+    });
+
+    const selectedCategories = selectedGroups.value.map(
+        (selectedGroup) => {
+            const footprint = footprintsData.value.find(
+                (f) => f.name === selectedGroup
+            );
+            return footprint!.name;
+        }
+    );
+
+    const available_Categories = footprintsData.value[0].values.map(
+        (valueObj) => valueObj.category
+    );
+
+    // extras
+    selectedData.push(
+        averageData.value.map((valueObj) => ({
+            category: valueObj.category,
+            value: valueObj.value,
+        }))
+    );
+    selectedCategories.push('Durchschnitt aller Beiträge');
+
     return {
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+                type: 'shadow',
+            },
+        },
+        legend: {
+            data: available_Categories,
+        },
         textStyle: {
             fontFamily: 'Inter, "Helvetica Neue", Arial, sans-serif',
             fontWeight: 300,
         },
-        dataset: {
-            dimensions: ['name', 'total'],
-            source: footprints_in_districts.value.map(
-                (districtFootprint: DistrictFootprint) => ({
-                    name: districtFootprint.name,
-                    total: districtFootprint.total,
-                })
-            ),
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true,
         },
-        xAxis: {},
-        yAxis: { type: 'category' },
-        series: [{ type: 'bar' }],
+        xAxis: {
+            type: 'category',
+            data: selectedCategories,
+            //selectedData[0].map((data) => data.category), // Use the categories from the first selectedData array
+        },
+        yAxis: {
+            type: 'value',
+            boundaryGap: [0, 0.01],
+        },
+        series: available_Categories.map((data: any, index: any) => ({
+            name: data,
+            type: 'bar',
+            // markline
+            markLine: {
+                label: {
+                    show: true,
+                    position: 'insideStart', //["10%","10%"], //"insideLeft", //'end',
+                    formatter: 'Ziel 2030:\n4,1 Tonnen\n\n\n', // Change this to your desired text
+                },
+                lineStyle: {
+                    width: 3,
+                    type: 'solid', // You can change the line style if needed
+                    color: 'gray', // You can change the line color
+                },
+                symbol: 'none',
+                data: [
+                    { yAxis: 4.1 }, // Change the yAxis value to set the position of the line
+                ],
+            },
+            //
+            stack: 'stack',
+            barWidth: '60%',
+            data: selectedData.map(
+                (selectedData: any) => selectedData[index].value
+            ),
+            label: {
+                show: true,
+                position: 'inside',
+                formatter: '{c}t CO2',
+            },
+        })),
     };
 }
 </script>
