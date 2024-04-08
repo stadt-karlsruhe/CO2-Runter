7406d363c4cb0dd50c9c17257b8c896189b5b11b
Kevin Beier <beierkevin2002@gmail.com>
2024-04-08 16:17:34 +0200

    added tips and tricks part per each QuestionsBlock.vue

--- client/src/components/Calculator/QuestionsBlock.vue
+++ client/src/components/Calculator/QuestionsBlock.vue
@@ -1,26 +1,28 @@
 <template>
-    <v-sheet border>
-        <div
-            v-for="(item, index) in questions.questions"
-            :key="index"
-            class="my-16 text-center"
-        >
-            <h3>{{ item.text }}</h3>
-            <v-select
-                v-model="item.selected.text"
-                :items="
-                    item.replies.map(
-                        (replies) => replies.text
-                    )
-                "
-                :return-object="true"
-                class="ma-16"
-                placeholder="Wählen Sie eine Option"
-                variant="outlined"
-                @update:modelValue="test(index)"
-            ></v-select>
-        </div>
-    </v-sheet>
+    <div
+        v-for="(item, index) in questions.questions"
+        :key="index"
+        class="my-16"
+    >
+        <h3 class="text-center">{{ item.text }}</h3>
+        <v-select
+            v-model="item.selected.text"
+            :items="item.replies.map((replies) => replies.text)"
+            :return-object="true"
+            class="mt-16"
+            placeholder="Wählen Sie eine Option"
+            variant="outlined"
+            @update:modelValue="test(index)"
+        ></v-select>
+
+        <v-alert type="info" variant="tonal" tex>
+            <template #title> Titel für Tips und Tricks </template>
+            <template #text>
+                Beschriebung zu Tips und Tricks als auch eine Verlinkung die
+                dann gemacht werden muss
+            </template>
+        </v-alert>
+    </div>
 </template>
 
 <script lang="ts" setup>
@@ -28,7 +30,7 @@ import { Category, Replies } from '@/types/Questionnaire';
 import useQuestions from '@/composables/useQuestions';
 
 const props = defineProps<{
-    categoryIndex: number,
+    categoryIndex: number;
     questions: Category;
 }>();
 
@@ -42,8 +44,11 @@ const test = (questionIndex: number) => {
     updateSelectedValue(props.categoryIndex, questionIndex, object!);
 };
 
-function findObjectInArray(array: Replies[], textValue: string): Replies | undefined {
-    return array.find(item => item.text === textValue);
+function findObjectInArray(
+    array: Replies[],
+    textValue: string
+): Replies | undefined {
+    return array.find((item) => item.text === textValue);
 }
 </script>
 <style scoped></style>
